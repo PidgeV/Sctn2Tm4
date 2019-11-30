78312f355114c8decc4227a9d1629d7100f7625e
Nathen Toms <45152858+NTomsNC@users.noreply.github.com>
2019-11-30 13:20:29 -0500

    GameManager

---
+++ Term Assignment/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	[SerializeField] private GameObject targetDummy;
+	[SerializeField] private List<Transform> enemySpawnPoints = new List<Transform>();
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		StartStage();
+	}
+
+	/// <summary>
+	/// Initialize a stage of the game
+	/// Load enemy, clear bullets etc 
+	/// </summary>
+	void StartStage()
+	{
+		foreach (Transform pos in enemySpawnPoints) {
+			GameObject.Instantiate(targetDummy, pos.position, pos.rotation);
+		}
+	}
+}
