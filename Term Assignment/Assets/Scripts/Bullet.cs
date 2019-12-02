d75f8d4fe38a5f2d5bf8d43bfe0fc01488083d41
Jack Maddigan <45188734+jmaddigan1@users.noreply.github.com>
2019-12-02 14:51:41 -0500

    asd

--- Term Assignment/Assets/Scripts/Bullet.cs
+++ Term Assignment/Assets/Scripts/Bullet.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-	float life = 5;
-	float counter = 0;
+    public float bulletLife = 10;
 
-	// Update is called once per frame
-	void Update()
-	{
-		counter += Time.deltaTime;
-		if (counter > life)
-		{
-			Destroy(gameObject);
-		}
-	}
+    private float bulletDuration = 0;
+    private float hitBoxSize = 1;
+    private float maxHitBoxSize = 1;
+
+    private bool resize = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //
+        bulletDuration += Time.deltaTime;
+
+        //
+        if (!resize && bulletDuration > 0.1f)
+            resize = true;
+
+        //
+        if (bulletDuration > bulletLife)
+            Destroy(gameObject);
+
+        //
+        if (resize && hitBoxSize < maxHitBoxSize)
+        {
+            hitBoxSize = Mathf.Lerp(hitBoxSize, maxHitBoxSize, 0.35f);
+            GetComponentInChildren<SphereCollider>().radius = hitBoxSize;
+        }
+    }
 }
