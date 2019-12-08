85d44f068a165f584446e7a9e1ea120b7447b9ed
Nathen Toms <45152858+NTomsNC@users.noreply.github.com>
2019-12-08 14:20:37 -0500

    Basic Minimap

---
+++ Term Assignment/Assets/MiniMap.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+	[SerializeField] private GameObject target;
+
+	// Update is called once per frame
+	void LateUpdate()
+	{
+		transform.position = target.transform.position + new Vector3(0, 50, 0);
+	}
+}
