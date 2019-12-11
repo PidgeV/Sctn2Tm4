4cfe6a74de63ce7b999db2cc1aff27cd82c7d7de
Reig219 <58218324+Reig219@users.noreply.github.com>
2019-12-10 22:14:50 -0500

    UI Implementation
    Changed the Canvas to use Screen Space Camera instead of Overlay. Made a health prefab. Started the UIManager Class

---
+++ Term Assignment/Assets/Scripts/UI/SpinInPlace.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinInPlace : MonoBehaviour
+{
+    public float rotSpeed = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(Vector3.back, rotSpeed * Time.deltaTime);
+    }
+}
